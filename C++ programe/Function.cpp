507667cc18429e018558af4ca66a2a68bac989cc
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-17 22:02:32 +0600

    Add files via upload

---
+++ C++ programe/Function.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int addition(int a,int b)
+{
+    int sum=a+b;
+    cout<<sum;
+}
+
+int subtraction(int a,int b)
+{
+    int sum1=a-b;
+    cout<<sum1;
+}
+int main()
+{
+    addition(10,20);
+    subtraction(20,10);
+
+}

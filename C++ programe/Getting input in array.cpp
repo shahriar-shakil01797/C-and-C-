b3dead285a3b99c8370f060106328b553c16392c
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-18 20:38:43 +0600

    Add files via upload

---
+++ C++ programe/Getting input in array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i,marks[10]={1,2,3,4,5,6,7,8,9,10};
+    do{
+
+        cout<<marks[i];
+        i++;
+    }while(1<=10);
+
+
+}
+

1c979274964ce2c66c9ab3df60cf52fae2ab34d7
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-19 22:02:58 +0600

    Add files via upload

---
+++ C++ programe/pointer.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+int main()
+{
+    int num=10;
+    int *p;
+
+     p=&num;
+
+    cout<<num<<endl;
+    cout<<&num<<endl;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+
+
+
+    return 0;
+}

5c4060fcd82d84e2affe17903d6e6729cf360ced
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-22 20:49:29 +0600

    Add files via upload

---
+++ c programe/Arithmetic Operator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Please Enter Two value = ");
+    scanf("%d %d",&a,&b);
+    int sum=a+b;
+    printf("The Summation is = %d",sum);
+    return 0;
+
+}

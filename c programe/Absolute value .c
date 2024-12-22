0fb838b69f72071967ff86f96986dc5859d627f8
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-22 20:49:09 +0600

    Add files via upload

---
+++ c programe/Absolute value .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int result,answer;
+    printf("Enter any negative number = ");
+    scanf("%d",&result);
+    answer=abs(result);
+    printf("The Answer is = %d",answer);
+    return 0;
+}

54453314528741f6798fb768ad21d118959f9747
Shahriar Shakil <shahriarshakil28@gmail.com>
2024-12-24 21:01:00 +0600

    Add files via upload

---
+++ c programe/Escape_sequence.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Mona\f");
+
+    printf("Mona\r");
+    printf("Mona\t");
+
+    printf("Mona\v");
+    printf("Mona\0");
+    printf("Mona\'");
+    printf("Mona\?");
+    printf("shakil\n");  //new line
+    printf("Nabil\n");
+    printf("Sami\a"); // allert bell
+
+
+
+
+    getch ();
+}

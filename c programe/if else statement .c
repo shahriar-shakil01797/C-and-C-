b9facc62d59838fcb278f6b4846cb14199fba352
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-01 09:39:41 +0600

    Add files via upload

---
+++ c programe/if else statement .c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+
+{
+    int number;
+    printf("Enter any number = ");
+    scanf("%d",&number);
+    if(number%2==0)
+        printf("Even");
+    else
+        printf("Odd");
+
+  return 0;
+}

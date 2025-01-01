c63d9582beb63863a1a0bf4c0becdbd6ac7d0f9e
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-01 09:39:11 +0600

    Add files via upload

---
+++ c programe/hexa_to_decimal.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter Any number which is hexa-decimal = ");
+    scanf("%x",&number);
+    printf("The Answer of is decimal number is =%d",number);
+    return 0;
+}

1cf4d994b6164f23a03415e856a0630c9f27a881
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-01 09:40:21 +0600

    Add files via upload

---
+++ c programe/if else statement1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int x,number=10;
+    printf("Enter Number = ");
+    scanf("%d",&x);
+
+    if(x==number)
+    {
+        printf("Hi\n");
+        printf("How are you Shakil");
+    }
+    else
+    {
+        printf("Sorry Shakil");
+    }
+    return 0;
+}
+

93fade360d7ed0b9057a9526ad1b3f3376de6bda
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-03 17:54:26 +0600

    Add files via upload

---
+++ c programe/Lower To Uppercase letter.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int lower;
+    printf("Enter Lowercase letter = ");
+    scanf("%c",&lower);
+    printf("The answer of Uppercase letter is = %c",lower-32);
+    return 0;
+}

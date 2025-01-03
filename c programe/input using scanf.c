e8ea758a2fc71df24e20ae2d38d5bc215f53052a
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-03 17:54:00 +0600

    Add files via upload

---
+++ c programe/input using scanf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+
+    printf("Enter Two Number = ");
+    scanf("%d %f",&a,&b);
+    printf("The Answer of two number : %d %.1f",a,b);
+    return 0;
+
+
+
+}

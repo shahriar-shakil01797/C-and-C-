98c6fdb68239ee2e9cd0af8aace6ed4fc3924c09
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-04 11:52:11 +0600

    Add files via upload

---
+++ c programe/octal_decimal.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+
+{
+    int number;
+    printf("Enter Any octal number = ");
+    scanf("%o",&number);
+    printf("The Answer os Decimal is = %d ",number);
+    return 0;
+
+
+
+}

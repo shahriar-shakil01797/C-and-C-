8783f1d99c928625ff2e48bad93f1a053715590f
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-06 22:14:49 +0600

    Add files via upload

---
+++ c programe/sizeof.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+    int a;
+    float b;
+    double c;
+    char d;
+
+    printf("Size of int = %d byte\n",sizeof(a));
+    printf("Size of int = %d byte\n",sizeof(b));
+    printf("Size of int = %d byte\n",sizeof(c));
+    printf("Size of int = %d byte\n",sizeof(d));
+
+    return 0;
+}

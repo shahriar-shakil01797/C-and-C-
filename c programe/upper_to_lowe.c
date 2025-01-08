73de19c5f8bc4fd70ad45f18a61b1e75e998accb
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-08 23:42:23 +0600

    Add files via upload

---
+++ c programe/upper_to_lowe.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+
+{
+   int upper;
+   printf("Enter Any uppercase letter = ");
+   scanf("%c",&upper);
+   printf("The Answer of lowercase :%c",upper+32);
+   return 0;
+
+}

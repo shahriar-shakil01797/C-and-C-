63c3a81ba04332e64b6ad0ac7bc8adf128edc4c9
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-10 23:44:05 +0600

    Add files via upload

---
+++ c programe/user_input1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+
+{
+    int a,b,c,sum;
+
+    printf("Please!enter three number = ");
+
+    scanf("%d %d %d",&a,&b,&c);
+    sum=a+b+c;
+    printf("The sum is = %d",sum);
+    return 0;
+
+
+
+}

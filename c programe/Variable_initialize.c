d0a8e5f49542249ccfc356b8bd1e58f4fda15eed
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-10 23:45:00 +0600

    Add files via upload

---
+++ c programe/Variable_initialize.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+int main()
+
+{
+
+    int a;
+    float b;
+    double c;
+    char ch;
+
+    a=10;
+    b=15.32;
+    c=20.321415;
+    ch='a';
+
+    printf("%d\n",a);
+
+    printf("%f\n",b);
+
+    printf("%lf\n",c);
+
+    printf("%c\n",ch);
+
+
+    getch();
+
+}

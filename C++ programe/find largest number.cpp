2bc978f856b9be1125f90c92ed70ada3f3efd024
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-13 22:18:43 +0600

    Add files via upload

---
+++ C++ programe/find largest number.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int Num1,num2;
+    cout<<"Plese!Enter the two number"<<endl;
+    cin>>Num1>>Num2;
+
+
+        if(Num1>Num2)
+        {cout<<"Numberis larger:="<<Num1<<endl;}
+
+
+
+        else if (Num2>Num1)
+        {cout<<"Number is larger:="<<Num2<<endl;}
+
+        return 0;
+}

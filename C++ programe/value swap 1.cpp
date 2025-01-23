86fe6ab8cc930ae3ab3a013e78fca9e760a9a052
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-23 11:52:04 +0600

    Add files via upload

---
+++ C++ programe/value swap 1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    int num1=5,num2=10;
+    int temp;
+
+    temp=num1;
+    num1=num2;
+    num2=temp;
+
+
+
+
+    cout<<"The Answer of num1 : = "<<num1;
+    cout<<"\nThe Answer of num2 : = "<<num2;
+    cout<<endl;
+
+    return 0;
+}

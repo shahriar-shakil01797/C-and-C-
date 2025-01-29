0e8f79f718116e4527fd2545b40971bfcfafac83
Shahriar Shakil <shahriarshakil28@gmail.com>
2025-01-29 21:43:38 +0600

    Add files via upload

---
+++ C++ programe/Swap value1.cpp
@@ -0,0 +1,24 @@
+// Here I write a program Swap value.
+
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+    int x=5,y=10;
+    int temp;
+
+    temp=x;
+    x=y;
+    y=temp;
+
+    cout<<"The Answer is x:= "<<x;
+    cout<<endl;
+    cout<<"The Answer is y:= "<<y;
+    cout<<endl;
+    cout<<endl;
+
+    return 0;
+
+}
